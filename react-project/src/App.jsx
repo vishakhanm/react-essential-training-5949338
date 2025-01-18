ef5674875abeba88e3ada2739f641d7415826bc9
vishakhanm <68022901+vishakhanm@users.noreply.github.com>
2025-01-18 01:02:43 +0000

    creating react components

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,43 +3,24 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function Header({name, year}) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>My React Project!</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <header>
+    <h1>{name}'s Kitchen</h1>
+    <p>Copyright {year}</p>
+    </header>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header name="Alex" year={2025}/>
+      <main>
+        <h2> We serve very delicious food </h2>
+      </main>
+    </div>
+  )
 }
 
 export default App;
