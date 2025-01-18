626f22a8bbd0be072f579d2ff1d2eb822d7775b6
vishakhanm <68022901+vishakhanm@users.noreply.github.com>
2025-01-18 01:17:15 +0000

    lists in react

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -9,13 +9,26 @@ function Header({ name, year }) {
   );
 }
 
+const items = [" Dosa",
+"Biryani",
+"Fries"
+];
+
+function Main({dishes}) {
+  return (
+    <ul>
+    {dishes.map((dish, i) => (
+      <li key={i} style={{ listStyleType: "none" }}>{dish}</li>
+    ))}
+    </ul>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Main dishes={items} />
     </div>
   );
 }
